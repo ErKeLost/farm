d3afa7406e416afba343fd08cf12827cbf9639a0
erkelost <1256029807@qq.com>
2023-04-23 22:34:24 +0800

    chore: test prettier

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -10,20 +10,21 @@ import { bindingPath, Config } from "../../binding/index.js";
 import { JsPlugin } from "../plugin/index.js";
 import { rustPluginResolver } from "../plugin/rustPluginResolver.js";
 import {
-  NormalizedServerConfig,
-  UserConfig,
-  UserHmrConfig,
-  UserServerConfig
+	NormalizedServerConfig,
+	UserConfig,
+	UserHmrConfig,
+	UserServerConfig,
 } from "./types.js";
+
 import { Logger } from "../logger.js";
 import { pathToFileURL } from "node:url";
 import { createHash } from "node:crypto";
 
 export * from "./types.js";
 export const DEFAULT_CONFIG_NAMES = [
-  "farm.config.ts",
-  "farm.config.js",
-  "farm.config.mjs"
+	"farm.config.ts",
+	"farm.config.js",
+	"farm.config.mjs",
 ];
 
 /**
@@ -32,155 +33,155 @@ export const DEFAULT_CONFIG_NAMES = [
  * @returns resolved config that parsed to rust compiler
  */
 export async function normalizeUserCompilationConfig(
-  userConfig: UserConfig,
-  mode: "development" | "production" = "production"
+	userConfig: UserConfig,
+	mode: "development" | "production" = "production",
 ): Promise<Config> {
-  const config: Config["config"] = merge(
-    {
-      input: {
-        index: "./index.html"
-      },
-      output: {
-        path: "./dist"
-      }
-    },
-    userConfig.compilation
-  );
-  config.coreLibPath = bindingPath;
-  const require = module.createRequire(import.meta.url);
-  const hmrClientPluginPath = require.resolve("@farmfe/runtime-plugin-hmr");
+	const config: Config["config"] = merge(
+		{
+			input: {
+				index: "./index.html",
+			},
+			output: {
+				path: "./dist",
+			},
+		},
+		userConfig.compilation,
+	);
+	config.coreLibPath = bindingPath;
+	const require = module.createRequire(import.meta.url);
+	const hmrClientPluginPath = require.resolve("@farmfe/runtime-plugin-hmr");
 
-  if (!config.runtime) {
-    config.runtime = {
-      path: require.resolve("@farmfe/runtime"),
-      plugins: []
-    };
-  }
+	if (!config.runtime) {
+		config.runtime = {
+			path: require.resolve("@farmfe/runtime"),
+			plugins: [],
+		};
+	}
 
-  if (!config.runtime.path) {
-    config.runtime.path = require.resolve("@farmfe/runtime");
-  }
+	if (!config.runtime.path) {
+		config.runtime.path = require.resolve("@farmfe/runtime");
+	}
 
-  if (!config.runtime.swcHelpersPath) {
-    config.runtime.swcHelpersPath = path.dirname(
-      require.resolve("@swc/helpers/package.json")
-    );
-  }
+	if (!config.runtime.swcHelpersPath) {
+		config.runtime.swcHelpersPath = path.dirname(
+			require.resolve("@swc/helpers/package.json"),
+		);
+	}
 
-  if (!config.runtime.plugins) {
-    config.runtime.plugins = [];
-  }
+	if (!config.runtime.plugins) {
+		config.runtime.plugins = [];
+	}
 
-  if (config.lazyCompilation === undefined) {
-    if (mode === "development") {
-      config.lazyCompilation = true;
-    } else {
-      config.lazyCompilation = false;
-    }
-  }
+	if (config.lazyCompilation === undefined) {
+		if (mode === "development") {
+			config.lazyCompilation = true;
+		} else {
+			config.lazyCompilation = false;
+		}
+	}
 
-  if (config.mode === undefined) {
-    config.mode = mode;
-  }
+	if (config.mode === undefined) {
+		config.mode = mode;
+	}
 
-  if (config.mode === "production") {
-    if (!config.output?.filename) {
-      config.output.filename = "[resourceName].[contentHash].[ext]";
-    }
-    if (!config.output?.assetsFilename) {
-      config.output.assetsFilename = "[resourceName].[contentHash].[ext]";
-    }
-  }
+	if (config.mode === "production") {
+		if (!config.output?.filename) {
+			config.output.filename = "[resourceName].[contentHash].[ext]";
+		}
+		if (!config.output?.assetsFilename) {
+			config.output.assetsFilename = "[resourceName].[contentHash].[ext]";
+		}
+	}
 
-  const normalizedDevServerConfig = normalizeDevServerOptions(
-    userConfig.server
-  );
+	const normalizedDevServerConfig = normalizeDevServerOptions(
+		userConfig.server,
+	);
 
-  if (
-    Array.isArray(config.runtime.plugins) &&
-    normalizedDevServerConfig.hmr &&
-    !config.runtime.plugins.includes(hmrClientPluginPath)
-  ) {
-    config.runtime.plugins.push(hmrClientPluginPath);
-  }
+	if (
+		Array.isArray(config.runtime.plugins) &&
+		normalizedDevServerConfig.hmr &&
+		!config.runtime.plugins.includes(hmrClientPluginPath)
+	) {
+		config.runtime.plugins.push(hmrClientPluginPath);
+	}
 
-  // we should not deep merge compilation.input
-  if (userConfig.compilation?.input) {
-    config.input = userConfig.compilation.input;
-  }
+	// we should not deep merge compilation.input
+	if (userConfig.compilation?.input) {
+		config.input = userConfig.compilation.input;
+	}
 
-  if (!config.root) {
-    config.root = userConfig.root ?? process.cwd();
-  }
+	if (!config.root) {
+		config.root = userConfig.root ?? process.cwd();
+	}
 
-  if (config.treeShaking === undefined) {
-    if (mode === "production") {
-      config.treeShaking = true;
-    } else {
-      config.treeShaking = false;
-    }
-  }
+	if (config.treeShaking === undefined) {
+		if (mode === "production") {
+			config.treeShaking = true;
+		} else {
+			config.treeShaking = false;
+		}
+	}
 
-  if (config.minify === undefined) {
-    if (mode === "production") {
-      config.minify = true;
-    } else {
-      config.minify = false;
-    }
-  }
+	if (config.minify === undefined) {
+		if (mode === "production") {
+			config.minify = true;
+		} else {
+			config.minify = false;
+		}
+	}
 
-  const plugins = userConfig.plugins ?? [];
-  const rustPlugins = [];
-  const jsPlugins = [];
+	const plugins = userConfig.plugins ?? [];
+	const rustPlugins = [];
+	const jsPlugins = [];
 
-  for (const plugin of plugins) {
-    if (typeof plugin === "string" || Array.isArray(plugin)) {
-      rustPlugins.push(await rustPluginResolver(plugin, config.root as string));
-    } else if (typeof plugin === "object") {
-      jsPlugins.push(plugin as JsPlugin);
-    }
-  }
+	for (const plugin of plugins) {
+		if (typeof plugin === "string" || Array.isArray(plugin)) {
+			rustPlugins.push(await rustPluginResolver(plugin, config.root as string));
+		} else if (typeof plugin === "object") {
+			jsPlugins.push(plugin as JsPlugin);
+		}
+	}
 
-  let finalConfig = config;
-  // call user config hooks
-  for (const jsPlugin of jsPlugins) {
-    finalConfig = (await jsPlugin.config?.(finalConfig)) ?? finalConfig;
-  }
+	let finalConfig = config;
+	// call user config hooks
+	for (const jsPlugin of jsPlugins) {
+		finalConfig = (await jsPlugin.config?.(finalConfig)) ?? finalConfig;
+	}
 
-  const normalizedConfig: Config = {
-    config: finalConfig,
-    rustPlugins,
-    jsPlugins
-  };
+	const normalizedConfig: Config = {
+		config: finalConfig,
+		rustPlugins,
+		jsPlugins,
+	};
 
-  return normalizedConfig;
+	return normalizedConfig;
 }
 
 export const DEFAULT_HMR_OPTIONS: Required<UserHmrConfig> = {
-  ignores: [],
-  host: "localhost",
-  port: 9801
+	ignores: [],
+	host: "localhost",
+	port: 9801,
 };
 
 export const DEFAULT_DEV_SERVER_OPTIONS: NormalizedServerConfig = {
-  port: 9000,
-  https: false,
-  // http2: false,
-  hmr: DEFAULT_HMR_OPTIONS
+	port: 9000,
+	https: false,
+	// http2: false,
+	hmr: DEFAULT_HMR_OPTIONS,
 };
 
 export function normalizeDevServerOptions(
-  options?: UserServerConfig
+	options?: UserServerConfig,
 ): NormalizedServerConfig {
-  if (!options) {
-    return DEFAULT_DEV_SERVER_OPTIONS;
-  }
+	if (!options) {
+		return DEFAULT_DEV_SERVER_OPTIONS;
+	}
 
-  if (options.hmr === true) {
-    options.hmr = DEFAULT_HMR_OPTIONS;
-  }
+	if (options.hmr === true) {
+		options.hmr = DEFAULT_HMR_OPTIONS;
+	}
 
-  return merge({}, DEFAULT_DEV_SERVER_OPTIONS, options);
+	return merge({}, DEFAULT_DEV_SERVER_OPTIONS, options);
 }
 
 /**
@@ -188,141 +189,141 @@ export function normalizeDevServerOptions(
  * @param configPath
  */
 export async function resolveUserConfig(
-  configPath: string,
-  logger: Logger
+	configPath: string,
+	logger: Logger,
 ): Promise<UserConfig> {
-  if (!path.isAbsolute(configPath)) {
-    throw new Error("configPath must be an absolute path");
-  }
+	if (!path.isAbsolute(configPath)) {
+		throw new Error("configPath must be an absolute path");
+	}
 
-  let userConfig: UserConfig = {};
-  let root: string = process.cwd();
+	let userConfig: UserConfig = {};
+	let root: string = process.cwd();
 
-  // if configPath points to a directory, try to find a config file in it using default config
-  if (fs.statSync(configPath).isDirectory()) {
-    root = configPath;
+	// if configPath points to a directory, try to find a config file in it using default config
+	if (fs.statSync(configPath).isDirectory()) {
+		root = configPath;
 
-    for (const name of DEFAULT_CONFIG_NAMES) {
-      const resolvedPath = path.join(configPath, name);
-      const config = await readConfigFile(resolvedPath, logger);
+		for (const name of DEFAULT_CONFIG_NAMES) {
+			const resolvedPath = path.join(configPath, name);
+			const config = await readConfigFile(resolvedPath, logger);
 
-      if (config) {
-        userConfig = config;
-        // if we found a config file, stop searching
-        break;
-      }
-    }
-  } else if (fs.statSync(configPath).isFile()) {
-    root = path.dirname(configPath);
+			if (config) {
+				userConfig = config;
+				// if we found a config file, stop searching
+				break;
+			}
+		}
+	} else if (fs.statSync(configPath).isFile()) {
+		root = path.dirname(configPath);
 
-    const config = await readConfigFile(configPath, logger);
+		const config = await readConfigFile(configPath, logger);
 
-    if (config) {
-      userConfig = config;
-    }
-  }
+		if (config) {
+			userConfig = config;
+		}
+	}
 
-  if (!userConfig.root) {
-    userConfig.root = root;
-  }
+	if (!userConfig.root) {
+		userConfig.root = root;
+	}
 
-  return userConfig;
+	return userConfig;
 }
 
 async function readConfigFile(
-  resolvedPath: string,
-  logger: Logger
+	resolvedPath: string,
+	logger: Logger,
 ): Promise<UserConfig | undefined> {
-  if (fs.existsSync(resolvedPath)) {
-    logger.info(`Using config file at ${chalk.green(resolvedPath)}`);
-    // if config is written in typescript, we need to compile it to javascript using farm first
-    if (resolvedPath.endsWith(".ts")) {
-      const Compiler = (await import("../compiler/index.js")).Compiler;
-      const hash = createHash("md5");
-      const outputPath = path.join(
-        os.tmpdir(),
-        "farmfe",
-        hash.update(resolvedPath).digest("hex")
-      );
-      const fileName = "farm.config.bundle.mjs";
-      const normalizedConfig = await normalizeUserCompilationConfig({
-        compilation: {
-          input: {
-            config: resolvedPath
-          },
-          output: {
-            filename: fileName,
-            path: outputPath,
-            targetEnv: "node"
-          },
-          external: [
-            ...module.builtinModules.map((m) => `^${m}$`),
-            ...module.builtinModules.map((m) => `^node:${m}$`)
-          ],
-          partialBundling: {
-            moduleBuckets: [
-              {
-                name: fileName,
-                test: [".+"]
-              }
-            ]
-          },
-          sourcemap: false,
-          treeShaking: false,
-          minify: false
-        },
-        server: {
-          hmr: false
-        }
-        // plugins: [
-        //   {
-        //     name: 'farm-plugin-external',
-        //     resolve: {
-        //       filters: {
-        //         importers: ['.+'],
-        //         sources: ['.+']
-        //       },
-        //       executor: (param) => {
-        //         // external all non-relative and non-absolute imports
-        //         if (
-        //           path.isAbsolute(param.source) ||
-        //           param.source.startsWith('.') ||
-        //           param.source.startsWith('@swc/helpers')
-        //         ) {
-        //           return null;
-        //         } else {
-        //           console.log('external', param.source);
-        //           return {
-        //             resolvedPath: 'external-path',
-        //             external: true,
-        //             sideEffects: false,
-        //             query: [],
-        //             meta: {}
-        //           };
-        //         }
-        //       }
-        //     }
-        //   }
-        // ]
-      });
-      const compiler = new Compiler(normalizedConfig);
-      await compiler.compile();
-      compiler.writeResourcesToDisk();
+	if (fs.existsSync(resolvedPath)) {
+		logger.info(`Using config file at ${chalk.green(resolvedPath)}`);
+		// if config is written in typescript, we need to compile it to javascript using farm first
+		if (resolvedPath.endsWith(".ts")) {
+			const Compiler = (await import("../compiler/index.js")).Compiler;
+			const hash = createHash("md5");
+			const outputPath = path.join(
+				os.tmpdir(),
+				"farmfe",
+				hash.update(resolvedPath).digest("hex"),
+			);
+			const fileName = "farm.config.bundle.mjs";
+			const normalizedConfig = await normalizeUserCompilationConfig({
+				compilation: {
+					input: {
+						config: resolvedPath,
+					},
+					output: {
+						filename: fileName,
+						path: outputPath,
+						targetEnv: "node",
+					},
+					external: [
+						...module.builtinModules.map((m) => `^${m}$`),
+						...module.builtinModules.map((m) => `^node:${m}$`),
+					],
+					partialBundling: {
+						moduleBuckets: [
+							{
+								name: fileName,
+								test: [".+"],
+							},
+						],
+					},
+					sourcemap: false,
+					treeShaking: false,
+					minify: false,
+				},
+				server: {
+					hmr: false,
+				},
+				// plugins: [
+				//   {
+				//     name: 'farm-plugin-external',
+				//     resolve: {
+				//       filters: {
+				//         importers: ['.+'],
+				//         sources: ['.+']
+				//       },
+				//       executor: (param) => {
+				//         // external all non-relative and non-absolute imports
+				//         if (
+				//           path.isAbsolute(param.source) ||
+				//           param.source.startsWith('.') ||
+				//           param.source.startsWith('@swc/helpers')
+				//         ) {
+				//           return null;
+				//         } else {
+				//           console.log('external', param.source);
+				//           return {
+				//             resolvedPath: 'external-path',
+				//             external: true,
+				//             sideEffects: false,
+				//             query: [],
+				//             meta: {}
+				//           };
+				//         }
+				//       }
+				//     }
+				//   }
+				// ]
+			});
+			const compiler = new Compiler(normalizedConfig);
+			await compiler.compile();
+			compiler.writeResourcesToDisk();
 
-      const filePath = path.join(outputPath, fileName);
-      // Change to vm.module of node or loaders as far as it is stable
-      if (process.platform === "win32") {
-        return (await import(pathToFileURL(filePath).toString())).default;
-      } else {
-        return (await import(filePath)).default;
-      }
-    } else {
-      // Change to vm.module of node or loaders as far as it is stable
-      if (process.platform === "win32") {
-        return (await import(pathToFileURL(resolvedPath).toString())).default;
-      } else {
-        return (await import(resolvedPath)).default;
-      }
-    }
-  }
+			const filePath = path.join(outputPath, fileName);
+			// Change to vm.module of node or loaders as far as it is stable
+			if (process.platform === "win32") {
+				return (await import(pathToFileURL(filePath).toString())).default;
+			} else {
+				return (await import(filePath)).default;
+			}
+		} else {
+			// Change to vm.module of node or loaders as far as it is stable
+			if (process.platform === "win32") {
+				return (await import(pathToFileURL(resolvedPath).toString())).default;
+			} else {
+				return (await import(resolvedPath)).default;
+			}
+		}
+	}
 }
