bf37ad7b35dc2c414ca9e57f14d64467c17b382b
erkelost <1256029807@qq.com>
2023-08-18 21:21:28 +0800

    chore: test jsonc

--- examples/tailwind/src/index.tsx
+++ examples/tailwind/src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import pkg from '../package.json';
+import a from '../aa.jsonc';
+console.log(pkg);
+console.log(a);
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
